93af5ce8622f26bc68a610fac3671754bf29ceb4
Ivan Juric <ijuric12@gmail.com>
2017-05-01 11:49:21 +0200

    init commit

---
+++ JudoTournamentBase/JudoTournamentBase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JudoTournamentBase.MvcApplication" Language="C#" %>
---
+++ JudoTournamentBase/JudoTournamentBase/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JudoTournamentBase.Startup))]
+namespace JudoTournamentBase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
